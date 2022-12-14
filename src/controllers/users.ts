3e8a143610707357f6ecc69c631cb4d79c1c64fa
Serhii Perehonchuk <serhii_perehonchuk@epam.com>
2022-12-14 15:43:02 +0200

    HT2 db is now UsersDB class

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,39 +1,40 @@
 import { RequestHandler } from 'express';
 
-import db from '../data-access/users';
+import UsersDB from '../data-access/users';
 import { User, UserCore, UsersSearchQuery } from '../types';
 
 type GetUserHandler = RequestHandler<{ userId: string }, User>;
 const getUser: GetUserHandler = async (req, res) => {
-    const user = await db.getUser(req.params.userId);
+    const user = await UsersDB.get(req.params.userId);
     if (!user) res.sendStatus(404);
     res.json(user);
 };
 
-type GetUsersHandler = RequestHandler<null, User[], null, UsersSearchQuery>;
+type GetUsersHandler = RequestHandler<{}, User[], null, UsersSearchQuery>;
 const getUsers: GetUsersHandler = async (req, res) => {
-    const users = await db.getUsers(req.query);
+    const params = req.query;
+    const users = await UsersDB.getAll(params);
     res.json(users);
 };
 
-type CreateUserHandler = RequestHandler<null, User, UserCore>;
+type CreateUserHandler = RequestHandler<{}, User, UserCore>;
 const createUser: CreateUserHandler = async (req, res) => {
     const userData = req.body;
-    const createdUser = await db.createUser(userData);
+    const createdUser = await UsersDB.create(userData);
     res.json(createdUser);
 };
 
 type UpdatedUserHandler = RequestHandler<{ userId: string }, User, UserCore>;
 const updateUser: UpdatedUserHandler = async (req, res) => {
     const userData = req.body;
-    const updatedUser = await db.updateUser(req.params.userId, userData);
+    const updatedUser = await UsersDB.update(req.params.userId, userData);
     if (!updatedUser) res.sendStatus(404);
     res.json(updatedUser);
 };
 
 type DeleteUserHandler = RequestHandler<{ userId: string }, User, UserCore>;
 const deleteUser: DeleteUserHandler = async (req, res) => {
-    const deletedUser = await db.deleteUser(req.params.userId);
+    const deletedUser = await UsersDB.delete(req.params.userId);
     if (!deletedUser) res.sendStatus(404);
     res.json(deletedUser);
 };
--- src/data-access/users.ts
+++ src/data-access/users.ts
@@ -1,69 +1,77 @@
 import { randomUUID } from 'crypto';
 import { User, UserCore, UsersSearchQuery } from '../types';
 import { findInstance } from '../utils/findInstance';
+import { sortByField } from '../utils/sortByField';
 import { MOCKED_USERS } from './mocks';
 
 const users = MOCKED_USERS;
 
-export function getUser(userId: User['id']): User | undefined {
-    return users.find((user) => user.id === userId && !user.isDeleted);
+export class UsersDB {
+    users: User[];
+
+    constructor() {
+        this.users = users;
+    }
+
+    public get(userId: User['id']): User | undefined {
+        return this.users.find((user) => user.id === userId && !user.isDeleted);
+    }
+
+    public getAll(params: UsersSearchQuery): User[] {
+        const limit = Number(params.limit);
+        const loginSubstring = params.loginSubstring;
+
+        let searchResult: User[] = [...users];
+
+        if (limit) searchResult = searchResult.slice(0, limit);
+
+        if (loginSubstring)
+            searchResult = users.filter((user) => {
+                return user.login.includes(loginSubstring);
+            });
+
+        searchResult = searchResult.sort(sortByField('login'));
+
+        searchResult = searchResult.filter((user) => !user.isDeleted);
+        return searchResult;
+    }
+
+    public create(data: UserCore): User {
+        const newUser = {
+            ...data,
+            id: randomUUID(),
+            isDeleted: false,
+        };
+        this.users.push(newUser);
+        return newUser;
+    }
+
+    public update(
+        userId: User['id'],
+        data: Partial<UserCore> | UserCore
+    ): User | undefined {
+        const [userIndex, user] = findInstance<User, string>(
+            users,
+            'id',
+            userId
+        );
+        if (!user && userIndex === -1) return undefined;
+        const updatedUser = { ...user, ...data } as User;
+        users[userIndex] = updatedUser;
+        return updatedUser;
+    }
+
+    public delete(userId: User['id']): User | undefined {
+        const [userIndex, user] = findInstance<User, string>(
+            users,
+            'id',
+            userId
+        );
+        if (!user && userIndex === -1) return undefined;
+        const deletedUser = { ...user, isDeleted: true } as User;
+        users[userIndex] = deletedUser;
+        return deletedUser;
+    }
 }
 
-export function getUsers(params: UsersSearchQuery): User[] {
-    const limit = Number(params.limit);
-    const loginSubstring = params.loginSubstring;
-
-    let searchResult: User[] = [...users];
-
-    if (limit) searchResult = searchResult.slice(0, limit);
-
-    if (loginSubstring)
-        searchResult = users.filter((user) => {
-            return user.login.includes(loginSubstring);
-        });
-
-    searchResult = searchResult.sort((a, b) => {
-        return a.login > b.login ? 1 : -1;
-    });
-
-    searchResult = searchResult.filter((user) => !user.isDeleted);
-    return searchResult;
-}
-
-export function createUser(data: UserCore): User {
-    const newUser = {
-        ...data,
-        id: randomUUID(),
-        isDeleted: false,
-    };
-    users.push(newUser);
-    return newUser;
-}
-
-export function updateUser(
-    userId: User['id'], //
-    data: Partial<UserCore> | UserCore
-): User | undefined {
-    const [userIndex, user] = findInstance<User, string>(users, 'id', userId);
-    if (!user && userIndex === -1) return undefined;
-    const updatedUser = { ...user, ...data } as User;
-    users[userIndex] = updatedUser;
-    return updatedUser;
-}
-
-export function deleteUser(userId: User['id']): User | undefined {
-    const [userIndex, user] = findInstance<User, string>(users, 'id', userId);
-    if (!user && userIndex === -1) return undefined;
-    const deletedUser = { ...user, isDeleted: true } as User;
-    users[userIndex] = deletedUser;
-    return deletedUser;
-}
-
-const db = {
-    getUser,
-    getUsers,
-    createUser,
-    updateUser,
-    deleteUser,
-};
-export default db;
+export default new UsersDB();
